9260906c954200cfde870c6cc2a1e41c876fcd9c
Sujicoding <117893004+Sujicoding@users.noreply.github.com>
2023-07-24 14:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+    this._width = width;
+    this._height = height;
+}
+	 get width() {
+    return this._width;
+  }
 
-class Square extends Animal {}
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Animal {
+	constructor(sideLength) {
+    // Call the parent class constructor with the sideLength argument for both width and height
+    super(sideLength, sideLength);
+  }
+
+  getPerimeter() {
+    return 4 * this.width; // In a square, all sides are equal, so we can use width or height
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
